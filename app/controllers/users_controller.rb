766f9dc5d08df553243e5e62dd9d44dca705a513
yizhu zong <agnes.lain@gmail.com>
2024-06-11 00:41:13 +0200

    update the params require & permit for User api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 
   def set_user
